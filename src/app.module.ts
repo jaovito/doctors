b8de16252e78f415a732229e4a514c21ae2c0102
jaovito <junior.vitorio.dias@gmail.com>
2021-06-20 16:11:02 -0300

    set default connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,12 @@ import { DoctorsModule } from './doctors/doctors.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SpecialtiesModule } from './specialties/specialties.module';
 import { DoctorsSpecialtiesModule } from './doctors-specialties/doctors-specialties.module';
-import { Doctor } from './doctors/entities/doctor.entity';
-import { Specialty } from './specialties/entities/specialty.entity';
-import { DoctorsSpecialty } from './doctors-specialties/entities/doctors-specialty.entity';
 
 @Module({
   imports: [
     SpecialtiesModule,
     DoctorsModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'docker',
-      database: 'doctors',
-      entities: [Doctor, Specialty, DoctorsSpecialty],
-      synchronize: false,
-    }),
+    TypeOrmModule.forRoot(),
     DoctorsSpecialtiesModule,
   ],
   exports: [TypeOrmModule],
